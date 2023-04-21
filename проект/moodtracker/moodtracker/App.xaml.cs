9f2c297153eb95840c7b2d1c2c49c5dcf3256300
victoriya64 <gabmul29@gmail.com>
2023-04-21 13:04:19 +0400

    проект

---
+++ проект/moodtracker/moodtracker/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace moodtracker
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        internal static Repositories.RepositoryBase db;
+        internal static string currentDate = DateTime.Now.ToString("yyyy-MM-dd");
+        internal static string selectedDate = DateTime.Now.ToString("yyyy-MM-dd  hh:mm");
+
+        public App()
+        {
+            db = new Repositories.RepositoryBase();
+        }
+    }
+}
---
+++ проект/moodtracker/moodtracker/Repositories/MoodLists.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moodtracker.Repositories
+{
+    public class MoodLists
+    {
+        public enum Mood { Rad = 0, Good = 1, Neutral = 2, Bad = 3, Awful = 4 }
+    }
+}
