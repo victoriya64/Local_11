0d0be5b423f143d9b980b470ff770577626781e5
victoriya64 <gabmul29@gmail.com>
2023-04-21 12:46:11 +0400

    3 задание

---
+++ 3 задание/z_3/z_3/Program.cs
@@ -0,0 +1,116 @@
+﻿using System.Data.SqlClient;
+using System;
+
+namespace z_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("1. Чтение из БД:");
+            string connectionString = "Server=(local); Database=ivlieva; Integrated Security=true";
+            string query = "SELECT * FROM Order1";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                Console.WriteLine("Таблица Order1:\n");
+                while (reader.Read())
+                {
+                    int idOrd = (int)reader["IdOrd"];
+                    int idCust = (int)reader["IdCust"];
+                    DateTime ordDate = (DateTime)reader["OrdDate"];
+                    Console.WriteLine("IdOrd: {0} IdCust: {1} OrdDate: {2}", idOrd, idCust, ordDate);
+                }
+                reader.Close();
+            }
+            query = "SELECT * FROM Customer";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                Console.WriteLine("\nТаблица Customer:\n");
+                while (reader.Read())
+                {
+                    int id = (int)reader["IdCust"];
+                    string FName = (string)reader["FName"];
+                    string LName = (string)reader["LName"];
+                    int idCity = (int)reader["IdCity"];
+                    string address = (string)reader["Address"];
+                    string zip = (string)reader["Zip"];
+                    string phone = (string)reader["Phone"];
+                    Console.WriteLine("IdCust: {0} FName: {1} LName: {2} IdCity: {3} Address: {4} Zip: {5} Phone: {6}", id, FName, LName, idCity, address, zip, phone);
+                }
+                reader.Close();
+            }
+            Console.WriteLine("\n2. Обработка и изменение данных из БД:");
+            Console.WriteLine("Изменим клиенту №5 (Марина Морозова) имя на Вероника Морозова:");
+            string updateQuery = "UPDATE Customer SET FName = @FName WHERE IdCust = @IdCust";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(updateQuery, connection);
+                command.Parameters.AddWithValue("@FName", "Вероника");
+                command.Parameters.AddWithValue("@IdCust", 5);
+                connection.Open();
+                int rowsAffected = command.ExecuteNonQuery();
+                Console.WriteLine("Rows affected: {0}", rowsAffected);
+            }
+            Console.WriteLine("\n3. Обработка и изменение данных из БД:");
+            Console.WriteLine("Добавим в таблицу Order1 новый заказ клиенту №5 (Вероника Морозова):");
+            string insertQuery = "INSERT INTO Order1 (IdCust) VALUES (@IdCust)";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(insertQuery, connection);
+                command.Parameters.AddWithValue("@IdCust", 5);
+                connection.Open();
+                int rowsAffected = command.ExecuteNonQuery();
+                Console.WriteLine("Rows affected: {0}", rowsAffected);
+            }
+            Console.WriteLine("\nИтог:");
+            query = "SELECT * FROM Order1";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                Console.WriteLine("Таблица Order1:\n");
+                while (reader.Read())
+                {
+                    int idOrd = (int)reader["IdOrd"];
+                    int idCust = (int)reader["IdCust"];
+                    DateTime ordDate = (DateTime)reader["OrdDate"];
+                    Console.WriteLine("IdOrd: {0} IdCust: {1} OrdDate: {2}", idOrd, idCust, ordDate);
+                }
+                reader.Close();
+            }
+            query = "SELECT * FROM Customer";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                Console.WriteLine("\nТаблица Customer:\n");
+                while (reader.Read())
+                {
+                    int id = (int)reader["IdCust"];
+                    string FName = (string)reader["FName"];
+                    string LName = (string)reader["LName"];
+                    int idCity = (int)reader["IdCity"];
+                    string address = (string)reader["Address"];
+                    string zip = (string)reader["Zip"];
+                    string phone = (string)reader["Phone"];
+                    Console.WriteLine("IdCust: {0} FName: {1} LName: {2} IdCity: {3} Address: {4} Zip: {5} Phone: {6}", id, FName, LName, idCity, address, zip, phone);
+                }
+                reader.Close();
+            }
+        }
+    }
+} 
---
+++ 3 задание/z_3_otchet/z_3_otchet/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace z_3_otchet
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadData();
+            if (dataTable == null)
+            {
+                LoadData();
+            }
+        }
+
+        private DataTable dataTable;
+        private void LoadData()
+        {
+            // подключение к базе данных
+            string connectionString = "Data Source=(local);Database=ivlieva;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            // запрос для выборки данных из таблиц Order1 и Customer
+            string query = "SELECT o.IdOrd, c.FName, c.LName, c.IdCity, c.Address, c.Zip, c.Phone, o.OrdDate " +
+                           "FROM Order1 o " +
+                           "JOIN Customer c ON o.IdCust = c.IdCust " +
+                           "GROUP BY YEAR(o.OrdDate), MONTH(o.OrdDate), c.IdCust, c.FName, c.LName, c.IdCity, c.Address, c.Zip, c.Phone, o.IdOrd, o.OrdDate " +
+                           "ORDER BY YEAR(o.OrdDate), MONTH(o.OrdDate), c.FName, c.LName";
+
+            // создание объекта SqlDataAdapter и заполнение DataGrid данными из запроса SQL
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+            dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            dataGrid.ItemsSource = dataTable.DefaultView;
+        }
+    }
+}
